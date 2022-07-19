89aba1a8ac0f9f91ca4a7bcfc547f055ff20f4ec
Eduardo Quirino <edu.3532quirino@gmail.com>
2022-07-19 14:33:05 -0300

    Envio de pastas originais

---
+++ CFB-Fessor Bruno/Programação Csharp/Constantes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ENTRADA
+            const string programador = "Eduardo Quirino";
+            const double pi = 3.1415;
+
+            //PROCESSAMNETO
+
+
+            //SAIDA
+            Console.WriteLine("programador: {0} \n pi: {1}",programador,pi);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CFB-Fessor Bruno/Programação Csharp/Operações de Bitwise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operações_de_Bitwise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Valpor de operações de Bitwise
+                    >>Dobra
+                    << Metade   
+            */
+
+            //ENTRADA 
+            int num = 10;
+
+            //PROCESSAMENTO
+            num = num >> 1;
+
+            //SAIDA
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
